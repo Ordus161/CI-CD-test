b1efae501e2d38c61c9f984a5d28a3413eff3f09
Maxim Bondaruk <makc1992@list.ru>
2023-01-18 09:45:57 +0300

    init

---
+++ src/main/java/org/example/springMvcApp/HelloController.java
@@ -0,0 +1,13 @@
+package org.example.springMvcApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "hello_world.html";
+    }
+}
