8e4a15ca36a4c37c17052b08d00763dcf25d3782
Maxim Bondaruk <makc1992@list.ru>
2023-01-20 16:41:54 +0300

    reconfig to Java-class config

--- src/main/java/org/example/springMvcApp/HelloController.java
+++ src/main/java/org/example/springMvcApp/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello(){
-        return "hello_world.html";
+        return "hello_world";
     }
 }
---
+++ src/main/java/org/example/springMvcApp/config/MyDispatcherServletInitializer.java
@@ -0,0 +1,18 @@
+package org.example.springMvcApp.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer { // !!! abstract class is using servlet-api, have to add dependency in pom
+
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
